88fb5f2c6a24263bed859ae46733be7e790d699c
euskalvirus <euskalvirus@gmail.com>
2020-04-01 20:05:28 +0200

    primera version de la pagina statica

---
+++ Prueba_Alumnos_Profesores/appcliente/js/main2.js
@@ -0,0 +1,6 @@
+console.info('esto lo puedes ver depurando');
+console.debug('es una traza de debug');
+let nombre = 'asdasd';
+console.trace(`Esto suele ser para tracear o decir ${nombre}`);
+console.warn('mensaje de warking');
+console.error('mensaje de error')
